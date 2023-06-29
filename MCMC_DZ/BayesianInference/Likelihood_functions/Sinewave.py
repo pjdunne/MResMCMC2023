b79523da8a582a3b7bdb04446a9d9aa937d622bf
VincenVN <90444151+VincenVN@users.noreply.github.com>
2023-06-29 17:52:51 +0100

    I have Generalized the process to calculate the FakeData and the likelihood function

--- MCMC_DZ/BayesianInference/Likelihood_functions/Sinewave.py
+++ MCMC_DZ/BayesianInference/Likelihood_functions/Sinewave.py
@@ -1,9 +1,8 @@
 from typing import List
 import numpy as np
-import scipy.special as spc
 
 class Sinewave_function:
-    def __init__(self, A: float, B: float, C: float, X_range: List[List[float]]):
+    def __init__(self, params: np.array, X_range: List[List[float]]):
 
         """
 
@@ -19,10 +18,7 @@ class Sinewave_function:
 
         """
 
-
-        self.A = A
-        self.B = B
-        self.C = C
+        self.params = params
         self.X_range = X_range
 
     def f(self, X: np.array):
@@ -42,88 +38,13 @@ class Sinewave_function:
         
         """
 
-
-        if (len(X.shape)==1):
-            X0_in_range = np.logical_and(X[0] >= self.X_range[0][0], X[0] <= self.X_range[0][1])
-            X1_in_range = np.logical_and(X[1] >= self.X_range[1][0], X[1] <= self.X_range[1][1])
-            if (X0_in_range & X1_in_range): 
-                return (self.A * np.sin(self.B * (np.sqrt(X[0] ** 2 + X[1] ** 2) - self.C)) + self.A)
-            else:
+        [self.B, self.C] = self.params
+        self.A = 1
+        if (self.C<(-np.pi) or self.C>np.pi):
+            if (len(X.shape)==1):
                 return 0
-        else:
-            X0_in_range = np.logical_and(X[:, 0] >= self.X_range[0][0], X[:, 0] <= self.X_range[0][1])
-            X1_in_range = np.logical_and(X[:, 1] >= self.X_range[1][0], X[:, 1] <= self.X_range[1][1])
-            in_range = np.logical_and(X0_in_range, X1_in_range)
-            Res = np.zeros(X.shape[0])
-            Res[in_range] = (self.A * np.sin(self.B * (np.sqrt(X[in_range, 0] ** 2 + X[in_range, 1] ** 2) - self.C)) + self.A)
-            return Res
-
-
-class Sinewave_likeli:
-    def __init__(self, FD, A: float, X_range: List[List[float]]):
-
-        """
-
-        The Sinewave Function
-
-        Arguments
-        ---------
-        FD : the fake data for the Sinusoidal_likelihood function
-        C (float): the value of the paramter C
-        X_range (List[List[float]]): the range of the Sinusoidal Function
-     
-        Returns
-        -------
-        None
-
-        """
-
-
-        self.FD = FD
-        self.A = A
-        self.X_range = X_range
-
-    def l(self, params)->float:
-
-        """
-        
-        The likelihood function of the Sinewave Function
-
-        Arguments
-        ---------
-        params (np.array): the value of the parameters of the Sinewave function.
-
-        Returns
-        -------
-        (float): the likelihood value of the inputed parameter values of the Sinewave function.
-
-        """
-
-        self.B = params[0]
-        self.C = params[1]
-        if (self.B<0 or self.C<0 or self.C>np.pi):
-            return 0
-        self.lambda_theta = np.floor(self.pdf(np.column_stack((self.FD.x.flatten(), self.FD.y.flatten())))*((self.FD.Data_range[0][1]-self.FD.Data_range[0][0])*(self.FD.Data_range[1][1]-self.FD.Data_range[1][0])/(self.FD.bins)**2)*self.FD.scaler)
-        likeli = np.power(self.lambda_theta, self.FD.FakeData)*np.exp(-self.lambda_theta)/(spc.factorial(self.FD.FakeData))
-        return np.sum(likeli)
-        
-    def pdf(self, X: np.array):
-
-        """
-        
-        The main functon of the Sinewave Function
-
-        Arguments
-        ---------
-        X (np.array): The inputed value of the variable of the Sinewave Fucntion
-
-        Returns
-        -------
-        (np.array): The output value of the Sinewave Function respected to the inputed value X
-        
-        
-        """
-
+            else:
+                return np.zeros(X.shape[0])
 
         if (len(X.shape)==1):
             X0_in_range = np.logical_and(X[0] >= self.X_range[0][0], X[0] <= self.X_range[0][1])
--- MCMC_DZ/MCMC/Statistic_tools.py
+++ MCMC_DZ/MCMC/Statistic_tools.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.special as spc
 
 class FakeDataGen2D_Poisson:
     def __init__(self, pdf, Data_range, bins, scaler=1):
@@ -24,3 +25,47 @@ class FakeDataGen2D_Poisson:
         self.x, self.y  = np.meshgrid(x, y)
         self.FakeData = np.floor(self.pdf(np.column_stack((self.x.flatten(), self.y.flatten())))*((self.Data_range[0][1]-self.Data_range[0][0])*(self.Data_range[1][1]-self.Data_range[1][0])/(self.bins)**2)*self.scaler)
         self.FakeData = np.asanyarray([np.random.poisson(datai) for datai in self.FakeData])
+
+class LikeliFuncGen:
+    def __init__(self, Data, pdf):
+
+        """
+
+        The Likelihood Function Generator
+
+        Arguments
+        ---------
+
+        Data : the data set for the pdf function function
+     
+        Returns
+        -------
+        None
+
+        """
+
+
+        self.Data = Data
+        self.pdf = pdf
+
+    def l(self, params):
+
+        """
+        
+        The Likelihood Function
+
+        Arguments
+        ---------
+        params : the parameter values of the probability denstiy function
+
+        Returns
+        -------
+        (float)
+        
+        """
+
+        self.pdf.params = params
+        self.lambda_theta = np.floor(self.pdf.f(np.column_stack((self.Data.x.flatten(), self.Data.y.flatten())))*((self.Data.Data_range[0][1]-self.Data.Data_range[0][0])*(self.Data.Data_range[1][1]-self.Data.Data_range[1][0])/(self.Data.bins)**2)*self.Data.scaler)
+        likeli = np.power(self.lambda_theta, self.Data.FakeData)*np.exp(-self.lambda_theta)/(spc.factorial(self.Data.FakeData))
+        return np.sum(likeli)
+        
